d67a6fda87303ed0c134637bcf8681383ffc78b8
Hamer Iboshi <hamer.iboshi@gmail.com>
2017-02-17 01:06:45 -0200

    Testing Laravel (Redis) with socket.io

---
+++ redis-lesson/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Redis;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// 1. Publish event with redis
+// 2. Node.js = Redis subscribes to the Event
+// 3. the socket.io to emit to all clients
+
+Route::get('/', function () {
+    // return view('welcome');
+});
